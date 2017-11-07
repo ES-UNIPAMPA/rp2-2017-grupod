f0ba78ced1afa352387fac10d3760b9c53bd7d3c
Jarom Sambrana <jaromsambrana@alunos.unipampa.edu.br>
2017-11-07 21:55:55 -0200

    Vou commitar antes que dê erro de novo.

--- src/sistema/SistemaEbook.java
+++ src/sistema/SistemaEbook.java
@@ -2,7 +2,7 @@
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
-*/
+ */
 package sistema;
 
 import gerenciadores.GerenciadorDoSistema;
@@ -11,20 +11,39 @@ import midias.Ebook;
 import midias.Midia;
 
 /**
-* @author Jarom Sambrana
-*/
+ * @author Jarom Sambrana
+ */
 //Classe que gerencia os eBooks
-
 public class SistemaEbook extends SistemaGeral implements GerenciadorDoSistema {
-    private Ebook ebook;
-    
+
+    Ebook ebook;
+
     public SistemaEbook() {
         super();
-        }
-    
+    }
+
     @Override
     public Midia consultar(String titulo) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-        
+
+    @Override
+    public Midia excluir(Midia midia) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void editar(Midia midia) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Midia exibir(String titulo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void ordenar(List<Midia> midias) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
