b80ea206733f73ada00ef0a10766302da49cf0e6
Jarom Sambrana <jaromsambrana@alunos.unipampa.edu.br>
2017-11-07 21:40:15 -0200

    Sumiu o errinho aquele no SistemaEbook o/

--- src/sistema/SistemaEbook.java
+++ src/sistema/SistemaEbook.java
@@ -5,11 +5,10 @@
 */
 package sistema;
 
-import bancoDeMidias.Midia;
 import gerenciadores.GerenciadorDoSistema;
 import java.util.List;
-import javax.swing.JOptionPane;
 import midias.Ebook;
+import midias.Midia;
 
 /**
 * @author Jarom Sambrana
@@ -17,49 +16,15 @@ import midias.Ebook;
 //Classe que gerencia os eBooks
 
 public class SistemaEbook extends SistemaGeral implements GerenciadorDoSistema {
-   private Ebook ebook;
-
-   public SistemaEbook() {
-
-       super();
-   }
-   public static void escolherAcao(String acao) {
-       if (acao.equalsIgnoreCase ("cadastrar")) {
-           String genero = JOptionPane.showInputDialog("Qual o gênero do livro?");
-           String escritor = JOptionPane.showInputDialog("Qual o nome do escritor?");
-
-       }
-   }
-
-   @Override
-   public Midia cadastrar(Midia midia) {
-
-   }
-
-   @Override
-   public Midia consultar(String titulo) {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-
-   @Override
-   public Midia excluir(Midia midia) {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-
-   @Override
-   public void editar(Midia midia) {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-
-   @Override
-   public Midia exibir(String titulo) {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-
-   @Override
-   public void ordenar(List<Midia> midias) {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-
+    private Ebook ebook;
+    
+    public SistemaEbook() {
+        super();
+        }
+    
+    @Override
+    public Midia consultar(String titulo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+        
 }
-
